77e0e313e3f4749adf8b4126587e9c4a2561dd23
lys <134896841+Adrenaline613@users.noreply.github.com>
2026-02-21 04:34:26 +0800

    Integrate spindle feature extraction in inference
    Added spindle feature extraction to the inference process.

--- inference.py
+++ inference.py
@@ -11,6 +11,7 @@ from data.utils import butter_bandpass_filter, resample_data, load_edf, data_to_
 from model.metric import *
 from model.postprocessing import process_events
 from model.net import S3Net
+from model.spindle_feature import spindle_feature
 
 
 def load_eeg(file_path, annot_path, channel='EEG(sec)', r_sf=100, is_filter=True, n2_only = True):
@@ -67,12 +68,16 @@ class InferDataset(Dataset):
 
 def main():
 
+    # Change the path here
     data_path = r'edf4test/shhs1-eeg-only.edf'
     annot_path = r'edf4test/shhs1-eeg-only-profusion.xml'
+
+    # Load data and prepare dataloader
     epochs = load_eeg(file_path=data_path, annot_path=annot_path, channel='EEG(sec)', r_sf=100, is_filter=True, n2_only=True)
     dataset = InferDataset(epochs)
     dataloader = DataLoader(dataset, batch_size=128, shuffle=False, drop_last=False)
 
+    # Load model
     ckpt_path = r'weight/MODA_S3Net_fullyfinetune_20250715/fold1_best.ckpt'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = S3Net(in_channels=1, num_classes=2, training_mode='scratch')
@@ -81,6 +86,8 @@ def main():
     model.load_state_dict(model_checkpoint['model_state_dict'])
     model.eval()
 
+    # Inference
+    all_inputs = []
     all_preds = []
     with torch.no_grad():
         for i, inputs in enumerate(dataloader):
@@ -91,11 +98,32 @@ def main():
 
             inputs = inputs.cpu().numpy()
             preds = preds.cpu().numpy()
+
+            # Postprocessing
             for idx, pred in enumerate(preds):
                 preds[idx] = process_events(pred, 100, 0.5, 3, 0.3)
+
+            all_inputs.append(inputs)
             all_preds.append(preds)
 
+    all_inputs = np.concatenate(all_inputs, axis=0).squeeze(1)
     all_preds = np.concatenate(all_preds, axis=0)
+
+    # Get spindle features
+    all_pred_sp_features = {
+        "Duration": [],
+        "Amplitude": [],
+        "Frequency": [],
+        "Oscillations": [],
+        "Symmetry": [],
+    }
+
+    for input, pred in zip(all_inputs, all_preds):
+        pred_sp_features = spindle_feature(input.astype(np.float64), pred, 100)
+        for key in all_pred_sp_features.keys():
+            all_pred_sp_features[key].extend(pred_sp_features[key])
+
+    print(all_pred_sp_features)
 
     return epochs, all_preds
 
@@ -103,6 +131,7 @@ def main():
 if __name__ == '__main__':
     all_epochs, all_preds = main()
 
+    # Visualize
     app = QApplication(sys.argv)
     viewer = EpochViewer(all_epochs, all_preds*50, (-200, 200))
     viewer.show()
